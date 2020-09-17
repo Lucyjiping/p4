1c72d0115ababb636e60cc670ea06d299c70e433
root <root@p4>
2020-09-17 16:29:32 +0000

    a script to get statistics of identifiying packets

---
+++ classify_pkt.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+
+import sys
+
+
+
+
+def main():
+
+	small_one = 0;
+	small_zero = 0;
+	big_one = 0;
+	big_zero = 0;
+	random_one = 0;
+	random_zero = 0;
+
+	if len(sys.argv)<2:
+		print("please input file name\n")
+		exit(1)
+
+	f = open(sys.argv[1])
+	for x in f:
+		y = f.readline()
+		if("hello" in y):
+			if("0x1" in x):
+				small_one = small_one + 1
+			elif("0x0" in x):
+				small_zero = small_zero +1
+
+		elif("big" in y):
+			if("0x3" in x):
+				big_one = big_one + 1
+			elif("0x0" in x):
+				big_zero = big_zero + 1
+		elif("greet" in y):
+			if("0x0" in x):
+				random_zero = random_zero + 1
+			elif("0x1" in x):
+				random_one = random_one + 1
+	f.close()
+
+	total_small = small_one + small_zero
+	total_big = big_one + big_zero
+	total_random = random_one + random_zero
+
+	small = "Small packets:\n	Total packet number:  {}\n	Correctly classified packet number: {}\n	Correct classified ratio: {:.2f}%\n\n"
+	print(small.format(total_small, small_one, small_one/total_small * 100));
+
+	big = "Bit packets:\n	Total packet number:  {}\n	Correctly classified packet number: {}\n	Correct classified ratio: {:.2f}% \n\n"
+	print(big.format(total_big, big_one, big_one/total_big * 100));
+
+	random_msg = "Random packets:\n	Total packet number:  {}\n	Wrongly classified packet number: {}\n	Wrongly classified ratio: {:.2f}% \n\n"
+	if(total_random != 0):
+		print(random_msg.format(total_random, random_one, random_one/total_random * 100));
+
+
+
+
+if __name__ == '__main__':
+	main()
+	
+
