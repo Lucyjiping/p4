982e6ef0ce19400929d26688587322737982d469
root <root@p4>
2020-10-24 00:30:28 +0000

    send small small large packets interval 0.03s

---
+++ send_003.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+import argparse
+import sys
+import socket
+import random
+import struct
+
+from scapy.all import sendp, send, get_if_list, get_if_hwaddr
+from scapy.all import Packet
+from scapy.all import Ether, IP, UDP, TCP
+
+from time import sleep
+
+def get_if():
+    ifs=get_if_list()
+    iface=None # "h1-eth0"
+    for i in get_if_list():
+        if "eth0" in i:
+            iface=i
+            break;
+    if not iface:
+        print "Cannot find eth0 interface"
+        exit(1)
+    return iface
+
+def main():
+
+    if len(sys.argv)<3:
+        print 'pass one arguments: <destination> <packet number> "'
+        exit(1)
+
+    addr = socket.gethostbyname(sys.argv[1])
+    iface = get_if()
+
+    print "sending on interface %s to %s" % (iface, str(addr))
+    pkt_t =  Ether(src=get_if_hwaddr(iface), dst='ff:ff:ff:ff:ff:ff')
+    pkt_t = pkt_t /IP(dst=addr) / TCP(dport=1234, sport=random.randint(49152,65535)) 
+    pkt_s = pkt_t /"hello"
+    pkt_l = pkt_t /"Really big world!"
+    pkt_f = pkt_t /"a"
+    pkt_f.show2()
+    sendp(pkt_f, iface=iface)
+    try:
+	for i in range(int(sys.argv[2])):	
+		for j in range(2):
+			pkt_s.show2()
+  			sendp(pkt_s, iface=iface)
+			sleep(0.03)
+		pkt_l.show2()
+		sendp(pkt_l, iface=iface)
+		sleep(0.03)
+    except KeyboardInterrupt:
+        raise
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
