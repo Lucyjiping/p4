9e9f058c080ac8727791706be46de33f3f7ad107
root <root@p4>
2020-11-08 04:35:55 +0000

    generate tos file command

---
+++ generate_tos_file.sh
@@ -0,0 +1,3 @@
+grep -E "tos|load" h3_audio_video_mix_100.log > h3_audio_video_mix_100_tos.log && grep -E "tos|load" h3_audio_video_mix_300.log > h3_audio_video_mix_300_tos.log && grep -E "tos|load" h3_audio_video_mix_500.log > h3_audio_video_mix_500_tos.log && grep -E "tos|load" h3_audio_video_mix_700.log > h3_audio_video_mix_700_tos.log
+
+
