45d555d935a9bb232fcba01d1556d165ac36a7cd
root <root@p4>
2020-11-17 05:06:32 +0000

    generate file commands for pod topology with 6hosts

---
+++ generate_tos_file_6hosts.sh
@@ -0,0 +1,3 @@
+grep -E "tos|load" h3_100.log > h3_100_tos.log && grep -E "tos|load" h3_300.log > h3_300_tos.log && grep -E "tos|load" h3_500.log > h3_500_tos.log && grep -E "tos|load" h3_700.log > h3_700_tos.log && grep -E "tos|load" h4_100.log > h4_100_tos.log && grep -E "tos|load" h4_300.log > h4_300_tos.log && grep -E "tos|load" h4_500.log > h4_500_tos.log && grep -E "tos|load" h4_700.log > h4_700_tos.log && grep -E "tos|load" h41_100.log > h41_100_tos.log && grep -E "tos|load" h41_300.log > h41_300_tos.log && grep -E "tos|load" h41_500.log > h41_500_tos.log && grep -E "tos|load" h41_700.log > h41_700_tos.log
+
+
